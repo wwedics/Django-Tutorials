ff4fbf24e97073c66df9e19791804af7466dfcff
wwedics <wwedics404@gmail.com>
2023-07-17 20:30:32 +0300

    done

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    location = models.CharField(max_length=402)
+    email = models.CharField(max_length=402)
+    phone = models.CharField(max_length=402)
+    name = models.CharField(max_length=402)
